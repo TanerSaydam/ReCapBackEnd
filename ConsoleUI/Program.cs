37c49e1d99e7ff475f4b467cb9871108974b8001
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2021-12-13 01:54:21 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,75 @@
+﻿using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Yeni kayıtlar oluşturuluyor
+            Car car1 = new Car();
+            car1.Id = 4;
+            car1.BrandId = 2;
+            car1.ColorId = 3;
+            car1.DailyPrice = 15000;
+            car1.Description = "Wolksvagen";
+
+            Car car2 = new Car();
+            car2.Id = 5;
+            car2.BrandId = 1;
+            car2.ColorId = 2;
+            car2.DailyPrice = 45000;
+            car2.Description = "Peugeot";
+
+            //Yeni kayıtlar inMemory üzerinden ekleniyor
+            InMemoryCarDal inMemoryCarDal = new InMemoryCarDal();
+            inMemoryCarDal.Add(car1);            
+            inMemoryCarDal.Add(car2);
+
+            Console.WriteLine("----------------------------------------");
+
+            //Car listesi ekrana yazdırılıyor
+            foreach (var item in inMemoryCarDal.GetAll())
+            {
+                Console.WriteLine(item.Description);
+            }
+
+            Console.WriteLine("----------------------------------------");
+
+            //car2 kayıtı listeden siliniyor.
+            inMemoryCarDal.Delete(car2);
+
+            Console.WriteLine("----------------------------------------");
+
+            //Car listesi tekrar ekrana yazdırılıyor
+            foreach (var item in inMemoryCarDal.GetAll())
+            {
+                Console.WriteLine(item.Description);
+            }
+
+            Console.WriteLine("----------------------------------------");
+
+            //car1'in bilgisi değiştirip sistemden update işlemi yapılıyor
+            car1.Description = "New Wolksvagen";
+            inMemoryCarDal.Update(car1);
+
+            Console.WriteLine("----------------------------------------");
+
+            //Car listesi tekrar ekrana yazdırılıyor
+            foreach (var item in inMemoryCarDal.GetAll())
+            {
+                Console.WriteLine(item.Description);
+            }
+
+            Console.WriteLine("----------------------------------------");
+
+            //Son olarak GetById ile 1 id'sine sahip kayıt bulunup ekrana bilgileri yazdırılıyor
+            var findCar = inMemoryCarDal.GetById(1);
+            Console.WriteLine(findCar.Description);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>()
+            {
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=1990,DailyPrice=10000,Description="Renault"},
+                new Car{Id=2,BrandId=1,ColorId=2,ModelYear=1997,DailyPrice=18000,Description="Mercedes"},
+                new Car{Id=3,BrandId=2,ColorId=2,ModelYear=2000,DailyPrice=19000,Description="Audi"},
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+            Console.WriteLine(car.Description + " kayıtlara eklendi.");
+        }
+
+        public void Delete(Car car)
+        {
+            Car carFind = _cars.Find(a => a.Id == car.Id);
+            _cars.Remove(carFind);
+            Console.WriteLine(car.Description + " kayıtlardan silindi.");
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int carId)
+        {
+            return _cars.Find(a => a.Id == carId);
+        }
+
+        public void Update(Car car)
+        {
+            Car carFind = _cars.Find(a => a.Id == car.Id);
+            _cars.Remove(carFind);
+            _cars.Add(carFind);
+            Console.WriteLine(car.Description + " kayıtı güncellendi.");
+        }
+    }
+}
